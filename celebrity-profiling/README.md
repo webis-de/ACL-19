1816a1a648d01437afa919f9c1f503ed92f37e1b
Matti Wiegmann <matti.wiegmann@uni-weimar.de>
2019-07-17 15:11:43 +0200

    add distribution and hydration utilities

---
+++ celebrity-profiling/README.md
@@ -0,0 +1,53 @@
+# Celebrity Profiling
+
+This repository contains the data and code for reproducing results of the paper:
+
+Matti Wiegmann, Benno Stein, and Martin Potthast. <a href="http://www.uni-weimar.de/medien/webis/publications/papers/stein_2018n.pdf" class="paper"><span class="title">Celebrity Profiling</span></a>. In <span class="booktitle">Proceedings of 57th Annual Meeting of the Association for Computational Linguistics (ACL 19)</span>, <span class="month">August</span> <span class="year">2019</span> 
+
+### Repository Overview
+
+In this repository you find:
+  - [x] The dehydrated distribution version of our celebrity profiling corpus,
+  - [x] Utilities to rehydrate this distribution,
+  - [ ] The code to reproduce this corpus from scratch, and
+  - [ ] The code to reproduce our experiments. 
+
+In accordance with Twitters policy, we can not make our archived version available publicly and you have to acquire all tweets yourself again. If you need access to our archived version or have any other inquiries, please contact the corresponding authors:
+  - Matti Wiegmann - matti.wiegmann[at]uni-weimar.de
+  - Martin Potthast - martin.potthast[at]uni-leipzig.de
+
+### Hydrating the Dataset
+
+1. Unzip the distribution file. 
+    ```bash
+    ~$ unzip webis-celebrity-corpus-2019-distribution.ndjson.zip
+    ```
+      This file contains on each line the Twitter ID of a celebrity account and all respective Wikidata properties:
+    ```json
+    {"id": 21447363, 
+     "labels": {
+       "sex or gender (P21)": "female (Q6581072)",
+       "genre (P136)": ["pop music (Q37073)", "rock music (Q11399)", "pop rock (Q484641)", ...], 
+       "ethnic group (P172)": ["English American (Q1344183)", "German American (Q141817)", ...], 
+       ...
+       },
+    }
+    ```
+2. Add your Twitter keys in the config.yml:
+    ```yaml
+    twitter_accounts:
+      - consumer_key: "my-consumer-key"
+        consumer_secret: "my-consumer-secret"
+        access_key: "my-access-key"
+        access_secret: "my-access-secret"
+
+    ```
+3. Install the requirements
+    ```bash
+    ~$ pip3 install -r requirements.txt
+    ```
+4. Run the hydrator:
+    ```bash
+    ~$ python3 hydrator.py 
+    ```
+    
---
+++ celebrity-profiling/hydrate.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from pathlib import Path
+import yaml
+from tweepy.api import API
+from tweepy import OAuthHandler, RateLimitError, Cursor
+from tweepy.error import TweepError
+from time import sleep
+import logging
+from math import inf
+
+
+def setup_environment(_config: dict):
+    """ make sure the files and paths required are available """
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Setup Environment")
+
+    _input_file = Path(_config.get("input_file", "./webis-celebrity-corpus-2019-distribution.ndjson"))
+    if not _input_file.exists():
+        logging.exception("Input distribution file given in the config does not exits, exiting.")
+        exit(1)
+
+    _output_path = Path(_config.get("output_path", "./hydrated"))
+    if not _output_path.is_dir():
+        _output_path.mkdir(parents=True)
+
+    if _config.get("aggregation", "compact") == "complete" and not Path(_output_path / "timelines").is_dir():
+        Path(_output_path / "timelines").mkdir()
+
+    return _input_file, _output_path
+
+
+def download_users(input_file, accounts):
+    for user in open(str(input_file), "r"):
+        u = json.loads(user)
+        logging.info("getting timeline for user %s" % u["id"])
+        try:
+            yield get_user(u["id"], accounts), get_timeline(u["id"], accounts)
+        except TweepError:
+            logging.info("user %s not available" % u["id"])
+
+
+def hydrate(input_file: Path, output_path: Path, accounts: list, aggregation: str):
+    """
+    Hydrate the celebrity corpus from the dehydrated version.
+    Parameters depend on values in the config.yaml when running this as __main__
+
+    :param input_file: The Path to the dehydrated file
+    :param output_path: the output directory, where to store the hydrated version
+    :param accounts: a list of Twitter access information
+    :param aggregation: "file_wise" one file per author, one line per tweet
+                        "compact" one file, one author per line
+    """
+    logging.info("Start hydration with %s accounts, \nReading from %s \nWriting output to %s",
+                 len(accounts), input_file, output_path)
+    for user_data, timeline in download_users(input_file, accounts):
+
+        if aggregation == "complete":
+            open(str(output_path / "users.ndjson"), "a").write(
+                json.dumps(user_data) + "\n")
+            open(str(output_path / "timelines" / ("%s.ndjson" % user_data["id"])), "w").writelines([
+                json.dumps(tweet) + "\n" for tweet in timeline
+            ])
+        elif aggregation == "compact":
+            open(str(output_path / "webis-celebrity-corpus-2019-hydrated.ndjson"), "a").write(
+
+                json.dumps({"id": user_data["id"], "statuses_count": user_data["statuses_count"],
+                            "screen_name": user_data["screen_name"], "lang": user_data["lang"],
+                            "followers_count": user_data["followers_count"], "name": user_data["name"],
+                            "timeline": [t["full_text"] for t in timeline]}) + "\n"
+            )
+        else:
+            logging.exception("Invalid aggregation method in the config, exiting")
+            exit(1)
+
+
+def get_api(accounts: list, limit_type: tuple = None):
+    """
+    return a tweepy api element, based on which account has a free limit.
+    If non is free, sleep here until it is.
+    If there is only one account configured, wait via tweepy
+    """
+    if len(accounts) == 1:
+        # there is only one account, so we don't consider account switching to circumvent rate limits
+        auth = OAuthHandler(accounts[0]["consumer_key"], accounts[0]["consumer_secret"])
+        auth.set_access_token(accounts[0]["access_key"], accounts[0]["access_secret"])
+        return API(auth, wait_on_rate_limit=True)
+
+    lowest_to_reset = inf
+    for account in accounts:
+        auth = OAuthHandler(account["consumer_key"], account["consumer_secret"])
+        auth.set_access_token(account["access_key"], account["access_secret"])
+        twitter = API(auth)
+        rate_limit = twitter.rate_limit_status()
+        if rate_limit['resources'][rate_limit[0]][rate_limit[1]]["remaining"] > 0:
+            return twitter
+        lowest_to_reset = min(lowest_to_reset, rate_limit['resources'][rate_limit[0]][rate_limit[1]]["reset"])
+    else:
+        logging.info("all rate limits reached, sleeping %s ms until reset", lowest_to_reset)
+        sleep(lowest_to_reset / 1000)
+        return get_api(accounts, limit_type)
+
+
+def get_user(user_id: int, accounts: list):
+    """ Get the latest Twitter user object of a given user_id and return. """
+    twitter = get_api(accounts, ('users', '/users/show/:id'))
+    return twitter.get_user(user_id)._json
+
+
+def get_timeline(user_id: int, accounts: list):
+    """ Get the latest Twitter timeline of the given user as a list of status responses. """
+    twitter = get_api(accounts, ('statuses', '/statuses/user_timeline'))
+    try:
+        return [status._json for status in Cursor(twitter.user_timeline, id=user_id, tweet_mode='extended').items()]
+    except RateLimitError as e:
+        logging.warning("RateLimitError while downloading a timeline, will try again with a different account. "
+                        "This may be a common event.", e)
+        return get_timeline(user_id, accounts)
+
+
+if __name__ == "__main__":
+    """
+    This is the hydrator script for the Webis Celebrity Corpus 2019, see https://github.com/webis-de/ACL-19
+
+    Usage:
+    (1) Make sure the config.yaml is correct.
+    (2) Install the requirements:
+      ~# pip install -r requirements.txt
+    (3) Run the script
+      ~# python3 hydrate.py 
+
+    Contact:
+    Matti Wiegmann - matti.wiegmann[at]uni-weimar.de
+    Martin Potthast - martin.potthast[at]uni-leipzig.de
+    Benno Stein - benno.stein[at]uni-weimar.de
+
+    https://www.webis.de
+    """
+    config = yaml.load(open("config.yaml", "r"), Loader=yaml.Loader)
+    inf, out = setup_environment(config)
+    hydrate(inf, out, config["twitter_accounts"], config.get("aggregation", "compact"))
